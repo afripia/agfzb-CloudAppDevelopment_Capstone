a74da52e4a63f2b04207ff7f356d9462bdc29a92
Mohammed Kedir <mkdrmash@gmail.com>
2022-04-02 13:20:36 +0000

    updated

--- functions/sample/nodejs/index-async-await.js
+++ functions/sample/nodejs/index-async-await.js
@@ -4,8 +4,15 @@
 
  const Cloudant = require('@cloudant/cloudant');
 
+ var params = {
+    "COUCH_USERNAME": "ceeedc5c-ae97-4b49-8621-08f97ef9c50d-bluemix",
+    "IAM_API_KEY": "yXZDE1xPrN2KF7m1kEvhm81GxUYz9Abo98b5--5z0Auk",
+    "COUCH_URL": "https://ceeedc5c-ae97-4b49-8621-08f97ef9c50d-bluemix.cloudantnosqldb.appdomain.cloud"
+}
 
- async function main(params) {
+
+//  async function main(params) {
+async function main() {
      const cloudant = Cloudant({
          url: params.COUCH_URL,
          plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
@@ -13,10 +20,25 @@
  
  
      try {
-         let dbList = await cloudant.db.list();
-         return { "dbs": dbList };
+         let dealerships = await new Promise(async resolve => {
+             let data = cloudant.db.use('dealerships');
+             let documents = await data.list({include_docs:true});
+             let results = []
+             if(documents.rows.length > 0){
+                for(let i = 0; i <= (documents.rows.length - 1); i++){
+                    results.push(documents.rows[i].doc);
+                }
+
+                resolve(results);
+            } else {
+                resolve([]);
+            }
+         });
+         return dealerships
      } catch (error) {
          return { error: error.description };
      }
  
- }
+}
+
+main()
--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -11,17 +11,27 @@ from cloudant.client import Cloudant
 from cloudant.error import CloudantException
 import requests
 
+dict = {
+    "COUCH_USERNAME": "ceeedc5c-ae97-4b49-8621-08f97ef9c50d-bluemix",
+    "IAM_API_KEY": "yXZDE1xPrN2KF7m1kEvhm81GxUYz9Abo98b5--5z0Auk",
+    "COUCH_URL": "https://ceeedc5c-ae97-4b49-8621-08f97ef9c50d-bluemix.cloudantnosqldb.appdomain.cloud"
+}
 
-def main(dict):
+# def main(dict):
+def main():
     databaseName = "dealerships"
 
     try:
         client = Cloudant.iam(
             account_name=dict["COUCH_USERNAME"],
             api_key=dict["IAM_API_KEY"],
+            url=dict["COUCH_URL"],
             connect=True,
         )
-        print("Databases: {0}".format(client.all_dbs()))
+        my_database = client[databaseName]
+        # print("Databases: {0}".format(client.all_dbs()))
+        # return {"dbs": client.all_dbs()}
+        return my_database
     except CloudantException as ce:
         print("unable to connect")
         return {"error": ce}
